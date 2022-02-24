7466a05b3a5bedaa7a0571dcd2ea89258cea9d8c
Muhammad Faizan <muhammad@tentwenty.me>
2022-02-24 17:21:51 +0500

    finished project

---
+++ android/app/src/main/kotlin/com/example/chips_types/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chips_types
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:chips_types/wrap_widgets.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: WrapWidgetDemo()));
+}
